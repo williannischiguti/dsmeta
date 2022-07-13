d1d8db8876b8cd08e2adb8bd328a97dac84324f6
Willian Nischiguti <williannischiguti@gmail.com>
2022-07-13 12:01:27 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/img/logo.svg'
+
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                    <h1>DSMeta</h1>
+                    <p>
+                        <a href="https://github.com/williannischiguti">github.com/williannischiguti</a>
+                    </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
