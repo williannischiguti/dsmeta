878d5ba0b431ec83ed7c1c009aba2634acf10b53
Willian Nischiguti <williannischiguti@gmail.com>
2022-07-15 13:57:35 -0300

    Ajustes no css do card

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -57,9 +57,9 @@ function SalesCard() {
                         <tr>
                             <th className="show992">ID</th>
                             <th className="show576">Data</th>
+                            <th>Vendedor</th>
                             <th className="show992">Visitas</th>
                             <th className="show992">Vendas</th>
-                            <th>Vendedor</th>
                             <th>Total</th>
                             <th>Notificar</th>
                         </tr>
